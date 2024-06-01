4e1b9338bacaca25d04d8956ffce25f25d3ff894
Aayush Shrestha <aayushshres20@gmail.com>
2024-06-01 09:49:14 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/walldrop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.walldrop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:walldrop/core/theme/app_pallete.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Walldrop",
+          style: GoogleFonts.pacifico(
+            textStyle: const TextStyle(
+              color: AppPallete.gradient1,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Home Page"),
+      ),
+    );
+  }
+}
